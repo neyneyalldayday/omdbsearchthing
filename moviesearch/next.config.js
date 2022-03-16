79e388f51d555d8778d9b03dd0c2f7305968855d
neyneyalldayday <drfrankenstein123@gmail.com>
2022-03-16 00:04:56 -0500

    not a fan of this api

--- moviesearch/next.config.js
+++ moviesearch/next.config.js
@@ -3,4 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    apiKey: process.env.API_KEY,
+  },
+  serverRuntimeConfig: {
+    apiKey: process.env.API_KEY,
+  },
+  publicRuntimeConfig: {
+    apiKey: process.env.API_KEY,
+  },
+  
+  
+  nextConfig
+
+}
--- moviesearch/pages/movedump.js
+++ moviesearch/pages/movedump.js
@@ -1,12 +1,15 @@
 
 
-export default function MovieDump({Title, index, Poster}) {
+export default function MovieDump({Title, index, Poster, Plot}) {
    
     return (
         <>
        <ul key={index}>
            <h3>{Title}</h3>
-           <img src={Poster} alt={Title} />            
+           <img src={Poster} 
+           alt={Title}
+           plot={Plot}
+           />                      
        </ul>
         </>
     )
